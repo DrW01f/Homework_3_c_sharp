7bdc02219f3598ed78775d51ab45e1f3b7b0c798
Andrey <Dr_Wo1f@mail.ru>
2023-02-03 22:26:18 +0300

    task 23 finished

---
+++ task23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N
+
+Console.Clear();
+
+Console.WriteLine ("Введите число");
+int count = int.Parse(Console.ReadLine()!);
+int i = 1;
+while (i < count)
+{
+    double result = Math.Pow(i,3);
+    Console.Write(result + ", ");
+    i ++;
+}
+Console.Write(Math.Pow(i,3)); //для выведения последнего числа без запятой
